8eb646d79bdf659a4c79b4a7af166001f24b4a91
Willian Rivera <willian.rivera@epn.edu.ec>
2024-07-08 19:39:32 -0500

    Ya integrado FrontEnd v0.1
    Aqui ya llamo del FrontEnd

--- app.py
+++ app.py
@@ -1,74 +1,208 @@
+import json
 import logging
 import os
-from flask import Flask, render_template, request, redirect, url_for
-from dotenv import load_dotenv
-from model.app_model import get_tuya_devices, get_firestore_devices
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+import pyrebase
+from dotenv import load_dotenv
+from flask import Flask, flash, redirect, render_template, request, url_for, jsonify
+from flask_cors import CORS
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_socketio import SocketIO, emit, join_room
+
+from model.app_model import get_tuya_devices, create_user, get_devices_for_user, get_all_users, \
+    update_device_status_tuya, db
+
+# Load environment variables from the .env file
 load_dotenv()
 
+# Initialize the Flask application
 app = Flask(__name__)
+# Configure the secret key for session management
 app.secret_key = os.getenv("SECRET_KEY")
-
+# Initialize the login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
+# Configure CORS to allow requests from localhost:5000
+CORS(app, origins=["http://localhost:5000"])
+# Initialize SocketIO for real-time communication
+socketio = SocketIO(app)
+
+# Initialize Firebase app with configuration from a JSON file
+pb = pyrebase.initialize_app(json.load(open('model/firebase/iot-dashboard-firebase.json')))
+
+# Get all users from the database
+users = get_all_users()
 
 
+# Define the User class for user handling with Flask-Login
 class User(UserMixin):
     pass
 
 
-users = {'foo': {'password': 'bar'}}
-
-
+# Define the function to load a user by their ID
 @login_manager.user_loader
-def user_loader(username):
-    if username not in users:
-        return
-
+def user_loader(user_id):
     user = User()
-    user.id = username
+    user.id = user_id
     return user
 
 
+# Route for handling login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = pb.auth().sign_in_with_email_and_password(email, password)
+            if user:
+                user_obj = User()
+                user_obj.id = user['localId']
+                login_user(user_obj)
+                return redirect(url_for('dashboard'))
+        except Exception as e:
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
 
-    username = request.form['username']
-    if username in users and request.form['password'] == users[username]['password']:
-        user = User()
-        user.id = username
-        login_user(user)
-        return redirect(url_for('dashboard'))
 
-    return 'Bad login'
-
-
-@app.route('/protected')
-@login_required
+# Route to display the dashboard with devices
+@app.route('/dashboard')
+@login_required  # Require login to access this route
 def dashboard():
+    return render_template('dashboard.html')
+
+
+# Route to display the dashboard with devices
+@app.route('/devices')
+@login_required  # Require login to access this route
+def devices():
     devices_tuya = get_tuya_devices()
-    devices_db = get_firestore_devices()
-    # logging.info("Response: %s", devices_tuya)
+    devices_db = get_devices_for_user(current_user.id)
     return render_template('index.html', devices_tuya=devices_tuya, devices_db=devices_db)
-    # return 'Logged in as: ' + current_user.id
 
+@app.route('/consumption')
+@login_required  # Require login to access this route
+def consumption():
+    # Aquí puedes obtener datos reales de consumo si es necesario
+    return render_template('consumption.html')
 
+# Route for handling logout
 @app.route('/logout', methods=['GET', 'POST'])
+@login_required
 def logout():
     logout_user()
-    return 'Logged out'
+    return redirect(url_for('login'))
 
 
+# Route for registering new users
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Check if passwords match
+        if password != confirm_password:
+            error = "Passwords do not match"
+            return render_template('registration.html', error=error)
+
+        try:
+            # Create the user
+            user_id = create_user(email, password)
+            user = User()
+            user.id = user_id
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        except Exception as e:
+            error = f"An error occurred: {e}"
+            return render_template('registration.html', error=error)
+    else:
+        return render_template('registration.html')
+
+
+# Route to get updated devices for the current user
+@app.route('/get_updated_devices')
+@login_required  # Require login to access this route
+def get_updated_devices():
+    devices_db = get_devices_for_user(current_user.id)
+    return jsonify(devices_db=devices_db)
+
+
+# Route to update the status of a device
+@app.route('/update_device_status', methods=['POST'])
+@login_required  # Require login to access this route
+def update_device_status():
+    device_id = request.form['device_id']
+    new_status = request.form['new_status'] == 'true'
+
+    try:
+        response = update_device_status_tuya(device_id, new_status)
+        if response:
+            device_ref = db.collection('Device').document(device_id)
+            device_ref.update({'status': [{'code': 'switch_1', 'value': new_status}]})
+            devices_db = get_devices_for_user(current_user.id)
+            socketio.emit('updated_devices', {'devices_db': devices_db}, namespace='/', room=current_user.id)
+            return jsonify(success=True)
+        else:
+            return jsonify(success=False, error="Failed to update status in Tuya")
+    except Exception as e:
+        logging.error(f"Error updating device status: {e}")
+        return jsonify(success=False, error=str(e))
+
+
+# Function to handle device updates from Firestore
+def on_device_update(docs, changes, read_time):
+    for change in changes:
+        if change.type.name == 'MODIFIED':
+            device = change.document.to_dict()
+            user_id = device.get('user_id')
+            if user_id:
+                current_status = device['status'][0]['value']
+                devices_db = get_devices_for_user(user_id)
+                # Emit dashboard update
+                socketio.emit('updated_devices', {'devices_db': devices_db}, namespace='/', room=user_id)
+
+                # Check if the status has actually changed before sending the POST request
+                device_ref = db.collection('Device').document(device['id'])
+                device_doc = device_ref.get()
+                if device_doc.exists:
+                    stored_status = device_doc.to_dict()['status'][0]['value']
+                    if stored_status != current_status:
+                        update_device_status_tuya(device['id'], current_status)
+                    else:
+                        # If the status hasn't changed, still send the POST request to ensure synchronization
+                        update_device_status_tuya(device['id'], current_status)
+
+
+# Register Firestore listener for updates in the 'Device' collection
+db.collection('Device').on_snapshot(on_device_update)
+
+
+# Handle new client connections via SocketIO
+@socketio.on('connect')
+def handle_connect():
+    if current_user and current_user.is_authenticated:
+        join_room(current_user.id)
+        devices_db = get_devices_for_user(current_user.id)
+        emit('updated_devices', {'devices_db': devices_db})
+    else:
+        logging.warning("No authenticated user found for handle_connect")
+
+
+# Root route that redirects to log in or dashboard depending on user's authentication status
 @app.route('/')
 def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     else:
         return redirect(url_for('login'))
 
 
+# Run the Flask application with support for SocketIO
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- model/app_model.py
+++ model/app_model.py
@@ -1,49 +1,39 @@
 # app_model.py
 
 import logging
+import os
+
 import firebase_admin
+from firebase_admin import firestore, credentials, auth
+from flask_login import LoginManager, current_user
+from flask_login import UserMixin
+from google.cloud.firestore_v1.base_query import FieldFilter
+
 from tuya_connector import TuyaOpenAPI, TUYA_LOGGER
-from firebase_admin import firestore, credentials
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('model/firebase/iot-dashboard-firebase.json')
 
+# Get the database URL from environment variables
+database_url = os.getenv("DATABASE_URL")
+
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://console.firebase.google.com/project/iot-dashboard-737c8/firestore'
+    'databaseURL': database_url
 })
 
-ACCESS_ID = "3ujca7y7apppqpcjjmdt"
-ACCESS_KEY = "e719ad0396c74b64bad8510c8baa491c"
-API_ENDPOINT = "https://openapi.tuyaus.com"
-DEVICE_ID = "vdevo170000581142241"
+# Initialize Firestore
+db = firestore.client()
+
+# Initialize the login manager
+login_manager = LoginManager()
 
 
-def get_tuya_devices():
-    TUYA_LOGGER.setLevel(logging.DEBUG)
-    openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
-    openapi.connect()
-
-    # Get the info of one device with ID
-    # response = openapi.get("/v1.0/iot-03/devices/{}".format(DEVICE_ID))
-
-    # Get all devices
-    response = openapi.get("/v2.0/cloud/thing/device?page_size=3")
-
-    # Lista para almacenar los IDs de los dispositivos
-    device_ids = []
-
-    # Extraer e almacenar el ID del dispositivo para cada dispositivo en la lista
-    for device in response.get("result", []):
-        device_id = device.get("id")
-        device_ids.append(device_id)
-
-    return device_ids
+class User(UserMixin):
+    pass
 
 
 def get_firestore_devices():
-    db = firestore.client()
-
     try:
         # Get reference to the "Device" collection
         devices_ref = db.collection('Device')
@@ -51,15 +41,155 @@ def get_firestore_devices():
         # Fetch all documents in the collection
         devices = devices_ref.stream()
 
-        # Lista para almacenar los IDs de los dispositivos
+        # List to store device IDs
         device_ids = []
 
-        # Recorrer cada documento y obtener su ID
+        # Iterate over each document and get its ID
         for device in devices:
             device_id = device.id
             device_ids.append(device_id)
 
         return device_ids
     except Exception as e:
-        print("Error al obtener los dispositivos de Firestore:", e)
+        print("Error retrieving devices from Firestore:", e)
         return []
+
+
+def create_user(email, password):
+    user = auth.create_user(email=email, password=password)
+    return user.uid
+
+
+def get_all_users():
+    # Get a reference to the auth service
+    auth = firebase_admin.auth
+
+    # Get the first page of users
+    page = auth.list_users()
+
+    # List to store user IDs
+    user_ids = []
+
+    # Iterate over all users in the page
+    for user in page.iterate_all():
+        user_ids.append(user.uid)
+
+    return user_ids
+
+
+def get_all_types():
+    # Get reference to the "Type" collection
+    types_ref = db.collection('Type')
+
+    # Fetch all documents in the collection
+    types = types_ref.stream()
+
+    # List to store types
+    type_list = [{'id': type_.id, 'name': type_.to_dict()['name']} for type_ in types]
+    return type_list
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    user = auth.get_user(user_id)
+    if user:
+        user_obj = User()
+        user_obj.id = user.uid
+        return user_obj
+    return None
+
+
+def add_device_to_firestore(device_id, device_info):
+    # Add or update a device in Firestore
+    device_ref = db.collection('Device').document(device_id)
+    device_ref.set(device_info, merge=True)
+
+
+def get_tuya_devices():
+    TUYA_LOGGER.setLevel(logging.DEBUG)
+    openapi = TuyaOpenAPI(os.getenv("API_ENDPOINT"), os.getenv("ACCESS_ID"), os.getenv("ACCESS_KEY"))
+    openapi.connect()
+
+    # Get all devices
+    response = openapi.get("/v2.0/cloud/thing/device?page_size=3")
+
+    # List to store device IDs
+    device_ids = []
+
+    # Extract and store the device ID for each device in the list
+    for device in response.get("result", []):
+        device_id = device.get("id")
+        device_info = device
+
+        # Make a GET request to the device's URL
+        device_status_response = openapi.get("/v1.0/iot-03/devices/{}/status".format(device_id))
+
+        # Store the response in Firebase
+        if device_status_response.get('success', False):
+            device_status = device_status_response.get('result', {})
+            device_info['status'] = device_status  # Assume the device status is in 'result'
+        else:
+            device_info['status'] = {}  # If the request fails, assume the device has no status
+
+        # Check if the device already exists in Firebase
+        device_ref = db.collection('Device').document(device_id)
+        device_doc = device_ref.get()
+
+        if device_doc.exists:
+            # If the device exists, update only the fields obtained from the Tuya API
+            device_ref.update(device_info)
+        else:
+            # If the device does not exist, add it to the database with the current user's ID
+            device_info['user_id'] = current_user.id
+            device_info['customName'] = device_info.get('customName', device_info.get('name'))  # Add customName field
+            device_ref.set(device_info)
+
+        device_ids.append(device_id)
+
+    return device_ids
+
+
+def add_device_to_user(user_id, device_name, device_type):
+    # Add a new device for the user in Firestore
+    device_ref = db.collection('Device').document()
+    device_ref.set({'name': device_name, 'type': device_type, 'user_id': user_id, 'is_active': False})
+
+
+def get_devices_for_user(user_id):
+    # Get all devices for a specific user
+    devices_ref = db.collection('Device')
+    devices = devices_ref.where(filter=FieldFilter('user_id', '==', user_id)).stream()
+    device_list = [{'id': device.id, **device.to_dict()} for device in devices]
+    return device_list
+
+
+def update_device_status_tuya(device_id, new_status):
+    TUYA_LOGGER.setLevel(logging.DEBUG)
+    openapi = TuyaOpenAPI(os.getenv("API_ENDPOINT"), os.getenv("ACCESS_ID"), os.getenv("ACCESS_KEY"))
+    openapi.connect()
+
+    # Get the current status of the device
+    device_status_response = openapi.get(f"/v1.0/iot-03/devices/{device_id}/status")
+
+    if device_status_response.get('success', False):
+        device_status = device_status_response.get('result', {})
+
+        # Find the command codes for switch_1 and switch_led
+        command_codes = [d['code'] for d in device_status if d['code'] in ['switch_1', 'switch_led']]
+
+        if not command_codes:
+            logging.error(f"Neither switch_1 nor switch_led found in device {device_id} status")
+            return False
+
+        # Create commands for each found command code
+        commands = {"commands": [{"code": code, "value": new_status} for code in command_codes]}
+        response = openapi.post(f'/v1.0/iot-03/devices/{device_id}/commands', commands)
+
+        if response.get('success', False):
+            return True
+        else:
+            logging.error(f"Failed to update device {device_id} status: {response}")
+            return False
+    else:
+        logging.error(f"Failed to get device {device_id} status: {device_status_response}")
+        return False
